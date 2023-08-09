d42c840c63adb7bdaa2451b714909b2bbfc956d4
Victor Hugo Rivero <108239738+VictorHugoRivero@users.noreply.github.com>
2023-08-09 18:26:27 -0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -5,3 +5,9 @@ darkModeToggle.addEventListener('click', () => {
     body.classList.toggle('dark-mode');
 });
 
+const darkModeToggle = document.querySelector('.dark-mode-toggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+});
